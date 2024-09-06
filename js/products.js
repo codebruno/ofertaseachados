28e766a45b16f828952df42e069c7f7c8f0598d5
codebruno <b.webdevs@gmail.com>
2024-09-06 11:55:33 -0300

    Inserindo os links dos produtos esportivos e kids

--- js/products.js
+++ js/products.js
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function() {
             // Adicione mais produtos conforme necessário
         ],
         "sports": [
-            { "title": "Bola de Futebol", "description": "Bola de futebol de alta qualidade, ideal para treinos e jogos.", "image": "https://via.placeholder.com/300x200", "link": "#", "stars": 4, "sold": 30 },
+            { "title": "Par de Meias Esportivas Antiderrapantes para Basquete - Conforto e Desempenho", "description": "Fabricadas com material respirável e solado reforçado para melhor desempenho durante os jogos.", "image": "assets/image/basquete.svg", "link": "https://s.shopee.com.br/2fs9nX1mXR", "stars": 5, "sold": 30 },
             { "title": "Bola de Futebol", "description": "Bola de futebol de alta qualidade, ideal para treinos e jogos.", "image": "https://via.placeholder.com/300x200", "link": "#", "stars": 4, "sold": 30 },
             { "title": "Bola de Futebol", "description": "Bola de futebol de alta qualidade, ideal para treinos e jogos.", "image": "https://via.placeholder.com/300x200", "link": "#", "stars": 4, "sold": 30 },
             { "title": "Bola de Futebol", "description": "Bola de futebol de alta qualidade, ideal para treinos e jogos.", "image": "https://via.placeholder.com/300x200", "link": "#", "stars": 4, "sold": 30 },
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', function() {
         ],
         "kids": [
             { "title": "Inalador AIQUE Ultra-Sônico Portátil", "description": "Nebulizador silencioso de grau médico com volume de névoa ajustável, ideal para tratamentos respiratórios práticos e eficientes.", "image": "assets/image/inalador.svg", "link": "https://s.shopee.com.br/9UiMZY1ZgH", "stars": 5, "sold": 32 },
-            { "title": "Brinquedo Educativo", "description": "Brinquedo educativo para crianças, estimula o aprendizado e a criatividade.", "image": "https://via.placeholder.com/300x200", "link": "#", "stars": 4, "sold": 55 },
+            { "title": "Termômetro Digital Infravermelho - Medição Precisa", "description": "Leitura rápida em 1 segundo com precisão de ±0,4°F. Recomendado por profissionais de saúde.", "image": "assets/image/termometro.svg", "link": "https://s.shopee.com.br/4pwcnq6nO2", "stars": 5, "sold": 55 },
             { "title": "Brinquedo Educativo", "description": "Brinquedo educativo para crianças, estimula o aprendizado e a criatividade.", "image": "https://via.placeholder.com/300x200", "link": "#", "stars": 4, "sold": 55 },
             { "title": "Brinquedo Educativo", "description": "Brinquedo educativo para crianças, estimula o aprendizado e a criatividade.", "image": "https://via.placeholder.com/300x200", "link": "#", "stars": 4, "sold": 55 },
             { "title": "Brinquedo Educativo", "description": "Brinquedo educativo para crianças, estimula o aprendizado e a criatividade.", "image": "https://via.placeholder.com/300x200", "link": "#", "stars": 4, "sold": 55 },
